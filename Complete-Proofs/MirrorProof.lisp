2c32134e4830104b7f1a5724ea42a9f5bafdfbc1
AndrewAngulo <82480195+AndrewAngulo@users.noreply.github.com>
2021-04-24 21:41:58 -0400

    Add files via upload

--- Complete-Proofs/MirrorProof.lisp
+++ Complete-Proofs/MirrorProof.lisp
@@ -1,78 +1,50 @@
 ; ****************** BEGIN INITIALIZATION FOR ACL2s MODE ****************** ;
 ; (Nothing to see here!  Your actual file is after this initialization code);
-(make-event
- (er-progn
-  (set-deferred-ttag-notes t state)
-  (value '(value-triple :invisible))))
 
-#+acl2s-startup (er-progn (assign fmt-error-msg "Problem loading the CCG book.~%Please choose \"Recertify ACL2s system books\" under the ACL2s menu and retry after successful recertification.") (value :invisible))
+#+acl2s-startup (er-progn (assign fmt-error-msg "Problem loading the CCG book.~%") (value :invisible))
 (include-book "acl2s/ccg/ccg" :uncertified-okp nil :dir :system :ttags ((:ccg)) :load-compiled-file nil);v4.0 change
 
 ;Common base theory for all modes.
-#+acl2s-startup (er-progn (assign fmt-error-msg "Problem loading ACL2s base theory book.~%Please choose \"Recertify ACL2s system books\" under the ACL2s menu and retry after successful recertification.") (value :invisible))
+#+acl2s-startup (er-progn (assign fmt-error-msg "Problem loading ACL2s base theory book.~%") (value :invisible))
 (include-book "acl2s/base-theory" :dir :system :ttags :all)
 
 
 #+acl2s-startup (er-progn (assign fmt-error-msg "Problem loading ACL2s customizations book.~%Please choose \"Recertify ACL2s system books\" under the ACL2s menu and retry after successful recertification.") (value :invisible))
-(include-book "acl2s/custom" :dir :system :ttags :all)
+(include-book "custom" :dir :acl2s-modes :ttags :all)
 
-;; guard-checking-on is in *protected-system-state-globals* so any
-;; changes are reverted back to what they were if you try setting this
-;; with make-event. So, in order to avoid the use of progn! and trust
-;; tags (which would not have been a big deal) in custom.lisp, I
-;; decided to add this here.
-;; 
-;; How to check (f-get-global 'guard-checking-on state)
-;; (acl2::set-guard-checking :nowarn)
-(acl2::set-guard-checking :all)
+#+acl2s-startup (er-progn (assign fmt-error-msg "Problem setting up ACL2s mode.") (value :invisible))
 
 ;Settings common to all ACL2s modes
 (acl2s-common-settings)
 ;(acl2::xdoc acl2s::defunc) ;; 3 seconds is too much time to spare -- commenting out [2015-02-01 Sun]
-
-#+acl2s-startup (er-progn (assign fmt-error-msg "Problem loading ACL2s customizations book.~%Please choose \"Recertify ACL2s system books\" under the ACL2s menu and retry after successful recertification.") (value :invisible))
-(include-book "acl2s/acl2s-sigs" :dir :system :ttags :all)
-
-#+acl2s-startup (er-progn (assign fmt-error-msg "Problem setting up ACL2s mode.") (value :invisible))
 
 (acl2::xdoc acl2s::defunc) ; almost 3 seconds
 
 ; Non-events:
 ;(set-guard-checking :none)
 
-(set-inhibit-warnings! "Invariant-risk" "theory")
-
-(in-package "ACL2")
-(redef+)
-(defun print-ttag-note (val active-book-name include-bookp deferred-p state)
-  (declare (xargs :stobjs state)
-	   (ignore val active-book-name include-bookp deferred-p))
-  state)
-
-(defun print-deferred-ttag-notes-summary (state)
-  (declare (xargs :stobjs state))
-  state)
-
-(defun notify-on-defttag (val active-book-name include-bookp state)
-  (declare (xargs :stobjs state)
-	   (ignore val active-book-name include-bookp))
-  state)
-(redef-)
-
 (acl2::in-package "ACL2S")
 
 ; ******************* END INITIALIZATION FOR ACL2s MODE ******************* ;
 ;$ACL2s-SMode$;ACL2s
+(cons 1 (cons 2 (cons 3 (cons 4 ()))))
+;; Which can be simplified to what is known
+;; as dot form, where the cons is replaced
+;; by a .
+'(1 . (2 . (3 . (4 . ()))))
 
-;; The mirror function reverses the construction of the input if it is a cons
-;; otherwise it returns the input
+;; ALL -> ALL
+;; The mirror function reverses the construction of the input
+;; if it is a cons otherwise it returns its input
 ;; this function recurs deeply, reversing the construction of any elements
 ;; of the list that are also cons'
 (definec mirror (tr :all) :all 
   (if (consp tr) 
-    (cons (mirror (cdr tr)) (mirror (car tr)))
+    (cons (mirror (cdr tr)) 
+          (mirror (car tr)))
     tr))
 
+;; Examples of mirror functionality
 (check= (mirror '(1 2 3)) '(((() . 3) . 2) . 1))
 (check= (mirror '()) '())
 (check= (mirror "apple") "apple")
@@ -80,12 +52,15 @@
 (check= (mirror '((1 2)(3 4)(5 6))) 
         '(((() (() . 6) . 5) (() . 4) . 3)(() . 2) . 1))
 
+
+;; TL X TL -> TL
 ;; Function which appends two lists
 ;; If the first list is empty, the second list is returned
 (definec app2 (a :tl b :tl) :tl
   (if (endp a)
       b
-    (cons (first a) (app2 (rest a) b))))
+      (cons (first a) 
+            (app2 (rest a) b))))
 
 (check= (app2 '(1 2 3) '(4 5 6)) '(1 2 3 4 5 6))
 (check= (app2 '() '(4 5 6)) '(4 5 6))
@@ -93,14 +68,17 @@
 (check= (app2 '() '()) '())
 (check= (app2 '(1 2 3) '((4 5 6)(7 8 9))) '(1 2 3 (4 5 6)(7 8 9)))
 
+;; TL -> TL
 ;; Function that returns the reverse of the input list
 ;; returns and empty list in the case where the input
 ;; list is empty
 (definec rev2 (x :tl) :tl
   (if (endp x)
       nil
-    (app2 (rev2 (rest x)) (list (first x)))))
+      (app2 (rev2 (rest x)) 
+            (list (first x)))))
 
+;; rev2 examples
 (check= (rev2 '(1 2 3)) '(3 2 1))
 (check= (rev2 '()) '())
 (check= (rev2 '(1 2 (3 4))) '((3 4) 2 1))
@@ -129,6 +107,17 @@
 ;; This can be proved inductively, where we assume that the (mirror (mirror (cdr x))) is 
 ;; equal to the (cdr x), however that is not enough for ACL2 to automatically assume the 
 ;; theorem
+
+;; Inductive Hypothesis 1
+(implies (tlp (cdr x))
+         (equal (mirror (mirror (cdr x))) (cdr x)))
+
+;; Inductive Hypothesis 2
+(implies (tlp (car x))
+         (equal (mirror (mirror (car x))) (car x)))
+
+;; Expansion of (mirror (mirror x)) in the inductive case
+(mirror (cons (mirror (cdr x)) (mirror (car x))))
 
 ;; In the expansion of (mirror x) in our lemma mirror-mirror can be expanded out to
 ;; (mirror (cons (mirror (cdr x)) (mirror (car x)))), which can be rewritten as 
@@ -159,15 +148,15 @@
 ;; And with this lemma proved, we can prove the target theorem as the reverses have no effect
 ;; on the output of the function which ACL2 can prove automatically by induction of (rev2 x)
 
-(defthm to-prove
+(defthm mirror-claim
   (implies (tlp x)
-           (equal (rev2 (mirror (mirror (rev2 x))))
-                  x)))#|ACL2s-ToDo-Line|#
+           (equal (rev2 
+                   (mirror (mirror (rev2 x))))
+                  x)))
 
-
-#| ;; This was used originally but is not necessary for proving the above theorem
+;;#| ;; This was used originally but is not necessary for proving the above theorem
 (defthm mirror-distr
   (implies (and (tlp x) (consp x))
            (equal (mirror (cons (mirror (cdr x)) (mirror (car x))))
                   (cons (mirror (mirror (car x))) (mirror (mirror (cdr x)))))))
-|#
+;;|#
